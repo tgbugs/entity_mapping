ff3acf27d8f3b949ac75f50f6f44781662301689
tsincomb <tsincomb@ucsd.edu>
2016-08-05 08:49:15 -0700

    should be ready for "path, rows"

--- troy.py
+++ troy.py
@@ -13,28 +13,32 @@ import csv
     Organizes a csv file to extract data easily
 """
 
-#folder = '/Users/love/git/troy_entity_mapping/'
+folder = '/Users/love/git/troy_entity_mapping/'#entity_mapping/nlx_154697_8_fma.csv'
 class csv_book():
 
-    def __init__(self, folder, filename):
+    #def __init__(self, folder, filename):
         # parses the csv file into a list of rows
-        def open_with_csv(filename):
-            with open(filename, 'r') as tsvin:
-                data = []
+        #For when a csv is being uploaded
+        #def open_with_csv(filename):
+         #   with open(filename, 'r') as tsvin:
+          #      data = []
                 # delimiter for weird symbols
-                tie_reader = csv.reader(tsvin)
-                for line in tie_reader:
-                    if line == "" or line == None or len(line) < 2:
-                        pass
-                    else:
-                        data.append(line)
-                return data
+           #     tie_reader = csv.reader(tsvin)
+            #    for line in tie_reader:
+             #       if line == "" or line == None or len(line) < 2:
+              #     else:
+                #        data.append(line)
+               # return data
         # organizes cells into hash; [y][x]
-        def data_dict():
+        # def data_dict(): //change back for csv uploading
+    def __init__(self, folder, rows):
+
+        def data_dict(rows):
             double_dict = {}
             single_dict = {}
             file_length = 0
-            for row_location, row in list(enumerate(open_with_csv(filename))):
+            #for row_location, row in list(enumerate(open_with_csv(filename))):
+            for row_location, row in list(enumerate(rows)):
                 file_length += 1
                 single_dict[row_location] = row
                 double_dict[row_location] = {}
@@ -43,8 +47,8 @@ class csv_book():
             return single_dict, double_dict, file_length
 
         self.folder = folder
-        self.filename = filename
-        self.rows, self.data, self.file_length = data_dict()
+        # self.filename = filename # for upload csv
+        self.rows, self.data, self.file_length = data_dict(rows)
         self.schema = {
             'source':0, 'table':1, 'column':2, 'value':2,  # loop variables
             'input_value':4, 'candidate':5, 'identifier':6, 'category':7, 'relation':8,
@@ -114,9 +118,33 @@ class csv_book():
     def printMultiMatch(self):
         print(self.provMultiList)
 
-    def makeCsvFromList(self, csvFilename):
+    def secondPassForSingle(self, value):
+        for row_number, row in list(enumerate(self.provSingleList)):
+            if value == row[self.schema_location("value")]:
+                self.provSingleList.remove(row)
+                return row
+        return None
+
+    def openMyCsvs(self, provType, id):
+        path = self.folder + 'entity_mapping/' + provType + "_" + id + '.csv'
+        fileLength = 0
+        with open (path, 'r') as file:
+            reader = csv.reader(file)
+            rows = []
+            for row in reader:
+                if row == "" or row == None or len(row) < 1:
+                    pass
+                else:
+                    fileLength += 1
+                    rows.append(row)
+        return rows, fileLength
+
+
+
+    def makeCsvFromList(self, id):
         for provTypes in self.provList:
-            with open (self.folder + 'entity_mapping/' + provTypes + "_" + csvFilename + '.csv', 'w') as file:
+            path = self.folder + 'entity_mapping/' + provTypes + "_" + id + '.csv'
+            with open (path, 'w') as file:
                 wr = csv.writer(file)
                 if provTypes == 'search':
                     for rows in self.provSearchList:
@@ -136,9 +164,11 @@ class csv_book():
             print ("done making", provTypes+".csv file")
 
 
-def makeSortedCSVFiles(folder, csvFileName):
+def makeSortedCSVFiles(path, rows):
     #source,table,column,value,input_value,candidate,identifier,fma_id,category,relation,prov,eid,ms,notes
-    csvFile = csv_book(folder, folder + 'entity_mapping/mappings/' + csvFileName + '.csv')
+    #csvFile = csv_book(folder, folder + 'entity_mapping/mappings/' + csvFileName + '.csv') #For csv upload
+
+    csvFile = csv_book(path, rows)
 
     """main"""
     for current_row_number in range(csvFile.file_length - 1):
@@ -161,7 +191,12 @@ def makeSortedCSVFiles(folder, csvFileName):
                     if csvFile.cell_from_index(crow, value).lower().split() == csvFile.cell_from_index(crow - 1, value).lower().split():
                         csvFile.addToMultiList(current_row_number)
                     else:
-                        csvFile.addToSingleList(current_row_number)
+                        test = csvFile.secondPassForSingle(value)
+                        if test != None:
+                            csvFile.addToMultiList(current_row_number)
+                            csvFile.addToMultiList(test)
+                        else:
+                            csvFile.addToSingleList(current_row_number)
                         #print(csvFile.cell_from_index(crow, value).lower().split(), csvFile.cell_from_index(crow + 1, value).lower().split())
             except:
                 if csvFile.cell_from_index(crow, value).lower().strip() == csvFile.cell_from_index(crow + 1, value).lower().strip():
@@ -169,13 +204,25 @@ def makeSortedCSVFiles(folder, csvFileName):
                 else:
                     csvFile.addToSingleList(current_row_number)
 
-    #csvFile.printMultiMatch()
-    csvFile.makeCsvFromList(csvFileName)
+    csvFile.makeCsvFromList(csvFile.cell_from_index(1, csvFile.schema_location("source")))
 
     #book_temp1 = csv_book(folder + 'entity_mapping/no_eids.csv')
-    book_temp1 = csv_book(folder, folder + 'entity_mapping/search_' + csvFileName + '.csv')
-    book_temp2 = csv_book(folder, folder + 'entity_mapping/no_eid_' + csvFileName + '.csv')
-    print("checking lengths of no_eid + multi + single/ied vs ori... ", csvFile.file_length - 1, 934 + book_temp2.file_length)
-    print("checking lengths of no_eid + search vs ori... ", csvFile.file_length - 1, book_temp1.file_length + book_temp2.file_length) # good :)
+    #provtypes = ['no_eid', 'search', 'one_match_or_has_eid', 'multi_match']
+    no_eid, no_eid_length = csvFile.openMyCsvs("no_eid", csvFile.cell_from_index(1, csvFile.schema_location("source")))
+    search, search_length = csvFile.openMyCsvs("search", csvFile.cell_from_index(1, csvFile.schema_location("source")))
+    one_match_or_has_eid, one_match_or_has_eid_length = csvFile.openMyCsvs("one_match_or_has_eid", csvFile.cell_from_index(1, csvFile.schema_location("source")))
+    multi_match, multi_match_length = csvFile.openMyCsvs("multi_match", csvFile.cell_from_index(1, csvFile.schema_location("source")))
 
-#makeSortedCSVFiles(folder, "nlx_154697_8_fma")
+    print("checking lengths of no_eid + multi + single/ied vs ori... ", csvFile.file_length - 1, no_eid_length + multi_match_length + one_match_or_has_eid_length)
+    print("checking lengths of no_eid + search vs ori... ", csvFile.file_length - 1, no_eid_length + search_length) # good :)
+
+with open('/Users/love/git/troy_entity_mapping/entity_mapping/mappings/nlx_154697_8_fma.csv', 'r') as file:
+    reader = csv.reader(file)
+    rows = []
+    for row in reader:
+        if row  == "" or row == None or len(row) < 1:
+            pass
+        else:
+            rows.append(row)
+makeSortedCSVFiles(folder, rows)
+
